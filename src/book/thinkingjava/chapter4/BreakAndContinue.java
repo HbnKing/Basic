45a588691e7442586cff16408b551d6832271bb0
henc <henc@wongdevice>
2017-12-11 13:28:21 +0800

    break  跳出整个循环 continue  跳出本次循环   进行下一次循环

---
+++ src/book/thinkingjava/chapter4/BreakAndContinue.java
@@ -0,0 +1,49 @@
+package book.thinkingjava.chapter4;
+
+import static  net.mindview.util.Range.*;
+/**
+ * break continue
+ * break  退出整个循环体  
+ * continue 退出本次循环
+ * @author HENC
+ *
+ */
+public class BreakAndContinue {
+	public static void main(String[] args) {
+		for(int i = 0 ;i <100 ;i++) {
+			if(i == 75 ) break;  //out of loop
+			if(i%9 !=0) continue;  //next Iteration
+			System.out.println(i);
+			
+		}	
+		System.out.println();
+		for(int i : range(100)) {
+			if(i == 74 ) break;
+			if(i%9!= 0 ) continue;
+			System.out.println(i);
+		}
+		
+		System.out.println("********");
+		int x = 0;
+		while(true) {
+			x++;
+			int y = x*27;
+			if(y == 1269) break;
+			if(x % 10 != 0) continue ;
+			System.out.println(x);
+		}
+		
+		System.out.println("********");
+		x = 0;
+		do {
+			x++;
+			int y = x*27;
+			if(y == 1269) break;
+			if(x % 10 != 0) continue ;
+			System.out.println(x);
+			
+		}while(true);
+	}
+	
+
+}
