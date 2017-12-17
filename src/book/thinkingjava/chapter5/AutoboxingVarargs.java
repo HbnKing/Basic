f8e189a8587e062e4dbffdb222902e4adf47e9b6
henc <henc@wongdevice>
2017-12-17 23:21:43 +0800

    可变参数与自动拆箱(封装类)

--- src/book/thinkingjava/chapter5/AutoboxingVarargs.java
+++ src/book/thinkingjava/chapter5/AutoboxingVarargs.java
@@ -9,11 +9,12 @@ public class AutoboxingVarargs {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		f(new Integer(10),new Integer(7));
+		f(1,2,3,4,5);
 	}
 	public static void f(Integer...args) {
 		for(Integer i : args)
-			System.out.print("i :"+i);
+			System.out.print("i :"+i +"  ");
 		System.out.println();
 		
 	}
