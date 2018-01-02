64e64e680844e92839e14783c1eb888efdd5021e
henc <henc@wongdevice>
2018-01-02 15:38:09 +0800

    outClassName.innerClassName 来创建 外部类

---
+++ src/book/thinkingjava/chapter10/Parcel2.java
@@ -0,0 +1,53 @@
+package book.thinkingjava.chapter10;
+
+
+
+/**
+ * 创建一个内部类
+ * 外部内的  方法 指向 内部类的引用  
+ * @author HENC
+ *
+ */
+public class Parcel2 {
+
+	class Contents{
+		private int i = 11;
+		public int value() {
+			return i;
+		}
+	}
+	class Destination{
+		private String label;
+		Destination(String whereTo){
+			label = whereTo;
+		}
+		String readLabel() {
+			return label;
+		}
+	}
+	
+	public Destination to(String S) {
+		return new Destination(S);
+	}
+	public Contents contents() {
+		return new Contents();
+	}
+	
+	public void ship(String dest) {
+	Contents c = new Contents();
+	Destination d = to(dest);
+	System.out.println(d.readLabel());
+	
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Parcel2 p = new Parcel2();
+		p.ship("666666");
+		Parcel2 q = new Parcel2();
+		Parcel2.Contents c = q.contents();
+		Parcel2.Destination d = q.to("Borneo");
+	}
+
+}
