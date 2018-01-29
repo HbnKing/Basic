dbf899b10b5ddba149d35f9c9db026002038eb40
Administrator <Administrator@V0TSPICAH39VEK2>
2018-01-29 11:23:27 +0800

    Iterable  和  Iterator

---
+++ src/book/thinkingjava/chapter11/ReversibleArrayList.java
@@ -0,0 +1,44 @@
+package book.thinkingjava.chapter11;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+/**
+ * 11.13.1
+ * 适配器的习惯用法
+ * @author Administrator
+ *
+ * @param <T>
+ * 
+ *说明 Iterable <> 的构造方法包含了 Iterator <> 
+ *
+ */
+public class ReversibleArrayList<T> extends ArrayList<T> {
+	public ReversibleArrayList(Collection<T> c) { super(c);}
+	public Iterable<T> reversed(){
+		return  new Iterable<T>() {
+			public Iterator<T> iterator(){
+				return new Iterator<T>() {
+					int current  = size() -1;
+					@Override
+					public boolean hasNext() {
+						// TODO Auto-generated method stub
+						return current > -1 ;
+					}
+
+					@Override
+					public T next() {
+						// TODO Auto-generated method stub
+						return get(current --);
+					}
+					public void remove() {
+						throw new UnsupportedOperationException();
+					}
+					
+				};
+			}
+
+		};
+		
+	}
+}
